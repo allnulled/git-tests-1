b1171bd5aa1e4fe1fdf29be2da36740683934414
allnulled <todosanulados@gmail.com>
2017-11-25 11:15:50 +0100

    Separated each function and alias. Changed "login" for "configuration". Added "gitConfigurationInfo" functionality.

--- gitCommands.sh
+++ gitCommands.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
-
-
 # ------------ Git-Login ---------------
-function gitLogin () {
-	echo "LOGIN"
+function gitConfiguration () {
+	echo "GIT Configuration"
     echo "Asking for your credentials"
     local USERNAME EMAIL PASSWORD
     read -p  " - Type the user's @name: " USERNAME 
@@ -15,28 +13,26 @@ function gitLogin () {
     git config user.email "$EMAIL"
     echo "Setting user.password to $PASSWORD"
     git config credential.helper store
-    echo "LOGIN"
+    echo "The credentials have been saved now for 1 day or so"
+    echo "This is your new configuration now:"
+    git config --list
+    echo "Thank you"
 }
-alias gitLogin="gitLogin"
-
-# ------------ Git-Login-Info ----------
-function gitLoginInfo () {
-	echo "LOGIN INFO"
-	git config
-	echo "LOGIN INFO"
+alias gitConfiguration="gitConfiguration"
+# ------------ Git-Configuration-Info ----------
+function gitConfigurationInfo () {
+	echo "GIT Configuration Info"
+	git config --list
+	echo "Thank you"
 }
 alias gitLoginInfo="gitLoginInfo"
-
 #------------- Git-Initialize ----------
-
 function gitInitialize () {
-	echo "INITIALIZE"
+	echo "GIT Initialize"
     git init
-	echo "INITIALIZE"
+	echo "Thank you"
 }
 alias gitInitialize="gitInitialize"
-
-
 #------------- Git-Create-Branch -------
 function gitCreateBranch () {
     local BRANCH BRANCH_VERSION BRANCH_ORIGIN BRANCH_ORIGIN_VERSION
@@ -47,11 +43,12 @@ function gitCreateBranch () {
     read -p " - Type the branch's version (default: last): " BRANCH_ORIGIN_VERSION
     echo "Creating branch $BRANCH. Wait, please..."
     git checkout $BRANCH $BRANCH_ORIGIN
-    read -p " - You want to switch to the new branch? " 
+    local CHANGE_BRANCH
+    read -p " - You want to switch to the new branch? " $CHANGE_BRANCH
+	echo "Thank you"
    
 }
 alias gitCreateBranch="gitCreateBranch"
-
 #------------- Git-Change-Branch -------
 function gitChangeBranch () {
 	local BRANCH BRANCH_ORIGIN
@@ -62,23 +59,27 @@ function gitChangeBranch () {
    
 }
 alias gitChangeBranch="gitChangeBranch"
-
-
+#------------- Git-Delete-Branch -------
 function gitDeleteBranch () {
     echo "Deleting branch"
     echo "Deleting branch $1"
     echo "Deleting branch $1 in its version $2"
-
 }
+alias gitDeleteBranch="gitDeleteBranch"
+#------------- Git-History-Branch -------
 function gitHistory () {
     echo "Checking history"
     echo "Checking history of file $1"
 }
+alias gitHistory="gitHistory"
+#------------- Git-Differences -------
 function gitDifferences () {
     echo "Differentiating"
     echo "Differentiating file $1"
     echo "Differentiating with program Melt"
 }
+alias gitDifferences="gitDifferences"
+#------------- Git-Commit -------
 function gitCommit () {
     echo "Commiting"
     echo "Commiting with message $1"
@@ -87,46 +88,54 @@ function gitCommit () {
     echo "Commiting files: x"
     echo "Is it okay? (Y/n)"
 }
+alias gitCommit="gitCommit"
+#------------- Git-Push -------
 function gitPush () {
     echo "Pushing"
 }
+alias gitPush="gitPush"
+#------------- Git-Mix-Branch -------
 function gitMixBranch () {
-    echo "Mixing"   
-    echo "Mixing with branch $1"    
+    echo "Mixing current branch"   
+    echo "Mixing current branch with branch $1"
+    echo "1. Copy current branch in a temporary folder" 
+    echo "2. Delete current branch" 
+    echo "3. Create same-named branch, but from the branch we want to mix"
+    echo "4. Add branch files to current same-named branch" 
 }
+alias gitMixBranch="gitMixBranch"
+#------------- Git-Status -------
 function gitStatus () {
-    echo "Showing status"
-    echo "Showing status of file $1"
+    echo "Showing commit status"
+    echo "Showing commit status of file $1"
 }
+alias gitStatus="gitStatus"
+#------------- Git-Add-File -------
 function gitAddFile () {
     echo "Adding file"
     echo "Adding file $1"
 }
+alias gitAddFile="gitAddFile"
+#------------- Git-Remove-File -------
 function gitRemoveFile () {
     echo "Removing file"
     echo "Removing file $1"
 }
+alias gitRemoveFile="gitRemoveFile"
+#------------- Git-Revert-File -------
 function gitRevertFile () {
     echo "Reverting file $1 to version $2"
 }
+alias gitRevertFile="gitRevertFile"
+#------------- Git-Revert-Branch -------
 function gitRevertBranch () {
     echo "Reverting branch $1 to version $2"
 }
+alias gitRevertBranch="gitRevertBranch"
+#------------- Git-Clone -------
 function gitClone () {
     echo "Cloning project"
     echo "Cloning project $1"
     echo "Cloning project $1 in version $2"
 }
-alias gitDeleteBranch="gitDeleteBranch"
-alias gitHistory="gitHistory"
-alias gitDifferences="gitDifferences"
-alias gitCommit="gitCommit"
-alias gitPush="gitPush"
-alias gitMixBranch="gitMixBranch"
-alias gitStatus="gitStatus"
-alias gitAddFile="gitAddFile"
-alias gitRemoveFile="gitRemoveFile"
-alias gitRevertFile="gitRevertFile"
-alias gitRevertBranch="gitRevertBranch"
 alias gitClone="gitClone"
-
